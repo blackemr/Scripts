fa3c76dee564d9e0eeae19079e3ca36ff4a4a21d
Matthew Blackert <39922552+blackemr@users.noreply.github.com>
2021-10-24 22:59:41 -0400

    Host Query Automation Tool
    Quickly iterate and store the nameserver, address record, IPV6 hosts, mail exchange, and the start of authority. Put the information into a file called output.txt for viewing.

---
+++ host-query.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+declare -a Commands=('ns' 'a' 'aaaa' 'mx' 'soa')
+echo "Scan for $1" >> output.txt
+for i in "${Commands[@]}"; do
+	echo "Section $i of host scan" >> output.txt
+	host -t "$i" $1 >> output.txt
+done
+printf "End scan for $1 \n\n" >> output.txt
